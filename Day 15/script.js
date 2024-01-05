967f7f9ebbfa6bc2cad8c6cfd32c6c578bd717f3
Motheo Motau <motheotb@gmail.com>
2024-01-05 20:11:24 +0200

    added day 16

--- Day 15/script.js
+++ Day 15/script.js
@@ -83,3 +83,51 @@ class Person {
     
   console.log(Person.favoriteSkill())
   console.log(Person.showDateTime())
+
+
+  // Inheritanece - Inherit methods and constructor from object
+  class Student extends Person {
+    constructor(firstName, lastName, age, country, city, gender) {
+      super(firstName, lastName, age, country, city)
+      this.gender = gender
+    }
+  
+    saySomething() {
+      return 'I am a child of the person class'
+    }
+
+    getPersonInfo() {
+      let fullName = this.getFullName()
+      let skills =
+        this.skills.length > 0 &&
+        this.skills.slice(0, this.skills.length - 1).join(', ') +
+          ` and ${this.skills[this.skills.length - 1]}`
+  
+      let formattedSkills = skills ? `He knows ${skills}` : ''
+      let pronoun = this.gender == 'Male' ? 'He' : 'She'
+  
+      let info = `${fullName} is ${this.age}. ${pronoun} lives in ${this.city}, ${this.country}. ${formattedSkills}`
+      return info
+    }
+  }
+  
+  const s1 = new Student('Asabeneh', 'Yetayeh', 'Finland', 250, 'Helsinki', 'Male')
+  console.log(s1)
+  console.log(s1.saySomething())
+  console.log(s1.getFullName())
+  console.log(s1.getPersonInfo())
+
+  const s2 = new Student('Lidiya', 'Tekle', 28, 'Finland', 'Helsinki', 'Female')
+s1.setScore = 1
+s1.setSkill = 'HTML'
+s1.setSkill = 'CSS'
+s1.setSkill = 'JavaScript'
+
+s2.setScore = 1
+s2.setSkill = 'Planning'
+s2.setSkill = 'Managing'
+s2.setSkill = 'Organizing'
+
+console.log(s2.saySomething())
+console.log(s2.getFullName())
+console.log(s2.getPersonInfo())
